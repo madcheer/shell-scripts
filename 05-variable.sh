f984b90ba8dbe6da88f9a9b947c9e8cc4ce69433
madan <madanmohan.devops@gmail.com>
2023-12-12 17:55:09 +0530

    read line

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "enter USER NAME:"
+
+echo "Please enter the user-name"
 read USERNAME
+echo "Please enter the password"
 read -s PASSWORD
 echo "user name is : $USERNAME"
 echo "password is : $PASSWORD"
