e28884c2da9542463642a26bf251d90497bff5ba
madan <madanmohan.devops@gmail.com>
2023-12-13 17:04:08 +0530

    validate function2

---
+++ 12-logs-validate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# redirecting to logfile
+# $? will give exit status of previous command 
+# $0 will give script name 
+
+VALIDATE()
+{
+
+if [ $1 -ne 0 ]
+   then
+         echo "$2..... is  FAILED"
+         exit 1
+   else
+          echo "$2..... is SUCCESSFUL"
+   fi 
+
+}
+
+ID=$(id -u)
+
+#to get script name
+
+echo "Script name $0"
+
+if [ $ID -ne 0 ]
+   then
+      echo " you are not a root user to install the software"
+      exit 1
+else
+       echo " you are a root user to install the software"
+fi
+
+yum install mysql -y
+
+VALIDATE $? $installing MYSQL
+    
+yum install git -y
+
+VALIDATE $? $installing GIT
+
