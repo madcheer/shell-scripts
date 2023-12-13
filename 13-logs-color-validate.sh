f3c936b048af98825d5b2dda94cec5d8f8acd702
madan <madanmohan.devops@gmail.com>
2023-12-13 17:41:55 +0530

    validate function2

---
+++ 13-logs-color-validate.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# redirecting to logfile
+# $? will give exit status of previous command 
+# $0 will give script name 
+
+VALIDATE()
+{
+
+if [ $1 -ne 0 ]
+   then
+         echo "$2..... is  $R FAILED"
+         exit 1
+   else
+          echo "$2..... is $G SUCCESSFUL"
+   fi 
+
+}
+
+ID=$(id -u)
+
+#to get script name with time as a log file
+
+TIMESTAMP=$(date +%F-%H-%M-%s)
+
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+if [ $ID -ne 0 ]
+   then
+      echo " you are not a root user to install the software"
+      exit 1
+else
+       echo " you are a root user to install the software"
+fi
+
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? $installing MYSQL
+    
+yum install git -y &>>$LOGFILE
+
+VALIDATE $? $installing GIT
+
