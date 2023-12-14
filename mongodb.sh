636a660610abf8c523b34ea298ed2dda2802ce63
madan <madanmohan.devops@gmail.com>
2023-12-14 16:40:25 +0530

    mogodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-VALIDATE()
-{
     R="\e[31m"
     G="\e[32m"
     Y="\e[33m"
     N="\e[0m"
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+  then
+      echo "you are not a root user to install"
+      exit 1
+  else
+    echo "you are root user"
+fi
+
+VALIDATE()
+{
 
 if [ $? -ne 0 ]
    then
@@ -17,22 +27,13 @@ fi
 
 }
 
-ID=$(id -u)
-if [ $ID -ne 0 ]
-  then
-      echo "you are not a root user to install"
-      exit 1
-  else
-    echo "you are root user"
-fi
+yum install mysql -y &>> 
 
-yum install mysql -y
-
-VALIDATE $?  "Installation of MYSQL"
+VALIDATE $?  "Installation of MYSQL" &>>$LOGFILE
 
 yum install httpd -y
  
-VALIDATE $?  "Installation of HTTPD"
+VALIDATE $?  "Installation of HTTPD" &>>$LOGFILE
 
 
    
