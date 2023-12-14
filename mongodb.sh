0244a9a667c3be34dc2fbd0116e3337836e8f1d6
madan <madanmohan.devops@gmail.com>
2023-12-14 14:40:37 +0530

    mogodb

--- mongodb.sh
+++ mongodb.sh
@@ -3,10 +3,10 @@
 ID=$(id -u)
 if [ $ID -ne 0 ]
   then
-      echo" you are not a root user to install"
+      echo "you are not a root user to install"
       exit 1
   else
-    echo " you are root user"
+    echo "you are root user"
 fi
 
 yum install mysql -y
