8d3208da1c4a3bf6f2753020765f25093361cb74
madan <madanmohan.devops@gmail.com>
2023-12-21 05:51:26 +0530

    shell-scripts-log rotation

---
+++ 15-log-rotation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/tmp/shellscripts-logs"
+
+if [ ! d $SOURCE_DIR]
+   then
+      echo -e "$R Souce directory does not exist"
+      exit 1
+fi
+
+FILES_TO_DELETE=$(find /tmp/shellscripts-logs -type f -mtime +14 -name "*.log")
+
+while IFS=read -r line
+    do
+       echo "deleting files $line"
+       rm -rf $line
+    done >>> $FILES_TO_DELETE
